0175a62c991285f077266899146ee4b542b1ceb5
Abhishek Pathak <abhishek.pathak@rea-group.com>
2024-08-26 22:26:48 +0530

    chapter 2: degenerate object

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -7,7 +7,11 @@ public class MoneyTest {
   @Test
   public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+
+    Dollar ten = five.times(2);
+    assertEquals(10, ten.amount);
+
+    Dollar fifteen = five.times(3);
+    assertEquals(15, fifteen.amount);
   }
 }
