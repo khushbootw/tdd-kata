89cf0ad2c9623ae0965c3381c6c2787b30331a3f
Khushboo2400 <khushboo.s2400@gmail.com>
2024-09-29 17:56:29 +0530

    chapter 6 : Equality for all, redux

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -6,26 +6,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneyTest {
 
-  @Test
-  public void testDollarMultiplication() {
-    Dollar five = new Dollar(5);
-    assertEquals(new Dollar(10), five.times(2));
-    assertEquals(new Dollar(15), five.times(3));
-  }
+    @Test
+    public void testDollarMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
 
-  @Test
-  public void testFrancMultiplication() {
-    Franc five = new Franc(5);
-    assertEquals(new Franc(10), five.times(2));
-    assertEquals(new Franc(15), five.times(3));
-  }
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 
-  @Test
-  public void testEquality() {
-    Dollar five = new Dollar(5);
-    Dollar anotherFive = new Dollar(5);
+    @Test
+    public void testDollarEquality() {
+        Dollar five = new Dollar(5);
+        Dollar anotherFive = new Dollar(5);
 
-    assertTrue(five.equals(anotherFive));
-    assertFalse(five.equals(new Dollar(10)));
-  }
+        assertTrue(five.equals(anotherFive));
+        assertFalse(five.equals(new Dollar(10)));
+    }
+
+    @Test
+    public void testFrancEquality() {
+        Franc five = new Franc(5);
+        Franc anotherFive = new Franc(5);
+
+        assertTrue(five.equals(anotherFive));
+        assertFalse(five.equals(new Franc(10)));
+    }
 }
